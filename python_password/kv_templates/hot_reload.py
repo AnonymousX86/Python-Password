ee0bb4847ff5cce7830e737a6d0c0282f6579e1d
AnonymousX86 <53279236+AnonymousX86@users.noreply.github.com>
2020-05-15 16:54:00 +0200

    Added ./kv_templates directory

---
+++ python_password/kv_templates/hot_reload.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+KV = '''
+#:import KivyLexer kivy.extras.highlight.KivyLexer
+#:import HotReloadViewer kivymd.utils.hot_reload_viewer.HotReloadViewer
+
+
+BoxLayout:
+
+    CodeInput:
+        lexer: KivyLexer()
+        style_name: "native"
+        on_text: app.update_kv_file(self.text)
+        size_hint_x: .7
+
+    HotReloadViewer:
+        size_hint_x: .3
+        path: app.path_to_kv_file
+        errors: True
+        errors_text_color: 1, 1, 0, 1
+        errors_background_color: app.theme_cls.bg_dark
+'''
